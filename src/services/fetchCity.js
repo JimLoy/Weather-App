975b533368815a079a8458312c97fb026c407bad
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-04-04 18:23:12 -0300

    dotenv error in fetchCity

--- src/services/fetchCity.js
+++ src/services/fetchCity.js
@@ -1,11 +1,11 @@
 import fetchData from "./fetch";
-require('dotenv').config()
+//require('dotenv').config()
 
-const apiKey = process.env.REACT_APP_APIKEY;
+//const apiKey = process.env.REACT_APP_APIKEY;
 
-export default function fetchCity(city, setData) {
+export default function fetchCity(city, setData) {   //&appid=${apiKey}
   fetchData(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`
   ).then(cityData => {
     if (cityData) setData(cityData);
     else setData(null);
